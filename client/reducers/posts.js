91243b72388cc52d399e4d53ae6eb40222ed0818
Ron de las Alas <ron.delasalas@gmail.com>
2017-03-18 21:49:26 -0400

    Add reducers to reduxstagram file

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,6 +3,9 @@
 // 2. copy of the current state
 
 function posts(state = [], action){
+    console.log("The post will change");
     console.log(state, action);
     return state;
 }
+
+export default posts;
