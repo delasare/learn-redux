c9c5e1ace95e0320031a94a75efbff171b07b292
Ron de las Alas <ron.delasalas@gmail.com>
2017-03-19 19:22:20 -0400

    Adds sagas to project

--- client/store.js
+++ client/store.js
@@ -1,11 +1,14 @@
-import { createStore, compose } from 'redux';
+import 'babel-polyfill';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
+import createSagaMiddleware from 'redux-saga';
 
 // import the root reducer
 import rootReducer from './reducers/index';
 import comments from './data/comments';
 import posts from './data/posts';
+import { helloSaga } from './sagas/sagas';
 
 // create an object for the default data
 const defaultState = {
@@ -16,10 +19,13 @@ const defaultState = {
 const enhancers = compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
 )
-
-const store = createStore(rootReducer, defaultState, enhancers);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, defaultState,applyMiddleware(sagaMiddleware), enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+sagaMiddleware.run(helloSaga);
+const action = type => store.dispatch({type})
 
 if(module.hot){
     module.hot.accept('./reducers/', ()=>{
